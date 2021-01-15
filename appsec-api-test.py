c8fd865798bcb64606ef6fe725c9a5579b37b830
sakuran1134 <none>
2021-01-15 16:21:20 +0900

    update appsec-api-test.py

--- appsec-api-test.py
+++ appsec-api-test.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import json
 from akamai.edgegrid import EdgeGridAuth
@@ -12,4 +13,16 @@ access_token='akab-2twz2lzoddpyzv3x-mp5piuk5y6ww3rtx'
 
 result = s.get(urljoin(baseurl, '/network-list/v2/network-lists?includeElements=truei&extended=true&listType=IP'))
 print(result.status_code)
-print(json.dumps(result.json(),indent=2))
+nw_str = json.dumps(result.json())
+nw_dict = json.loads(nw_str)
+
+api_uri = nw_dict['networkLists'][0]['links']['retrieve']['href']
+nwList_ips = s.get(urljoin(baseurl, api_uri))
+print(json.dumps(nwList_ips.json(),indent=2))
+
+#lines = nw_list.readlines()
+
+#l_XXX = [line for line in lines_strip if 'XXX' in line]
+#print(l_XXX)
+
+
